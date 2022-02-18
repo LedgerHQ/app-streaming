719d294a4593d032c292961a4eb130c47bf7ad44
greenknot <greenknot@users.noreply.github.com>
2022-02-18 17:03:07 +0100

    app: link against -lgcc

--- app/sdk/math.c
+++ app/sdk/math.c
@@ -68,3 +68,32 @@ unsigned int __udivsi3(unsigned int n, unsigned int d)
     q = (q << 1) | carry;
     return q;
 }
+
+/* https://chromium.googlesource.com/chromiumos/third_party/coreboot/+/firmware-paine-6301.58.B/src/arch/mips/ashldi3.c */
+typedef unsigned word_type;
+long long __ashldi3(long long u, word_type b);
+struct DWstruct {
+	int low, high;
+};
+typedef union {
+	struct DWstruct s;
+	long long ll;
+} DWunion;
+long long __ashldi3(long long u, word_type b)
+{
+	DWunion uu, w;
+	word_type bm;
+	if (b == 0)
+		return u;
+	uu.ll = u;
+	bm = 32 - b;
+	if (bm <= 0) {
+		w.s.low = 0;
+		w.s.high = (unsigned int) uu.s.low << -bm;
+	} else {
+		const unsigned int carries = (unsigned int) uu.s.low >> bm;
+		w.s.low = (unsigned int) uu.s.low << b;
+		w.s.high = ((unsigned int) uu.s.high << b) | carries;
+	}
+	return w.ll;
+}
