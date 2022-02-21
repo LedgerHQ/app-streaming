491a113666f535cfad8714ff5487fc25fca12684
greenknot <greenknot@users.noreply.github.com>
2022-02-21 16:32:27 +0100

    update docker.sh (PS1)

--- docker.sh
+++ docker.sh
@@ -11,5 +11,4 @@ if [ ! -f ${SCRIPT_DIR}/app/dockcross-linux-riscv32-latest ]; then
     chmod 0755 ${SCRIPT_DIR}/app/dockcross-linux-riscv32-latest
 fi
 
-
-docker run -w /app -v ${SCRIPT_DIR}/app/:/app/ --rm -it riscv bash
+docker run -u $(id -u):$(id -g) --env=SUDO_PS1=1 --env=SUDO_USER=1 --env=PS1='[\[\033[01;33m\]user@\h\[\033[00m\]:\[\033[01;33m\]\w\[\033[00m\]] \[\033[01;91m\]\$\[\033[01;35m\]\[\033[00m\] ' -w /app -v ${SCRIPT_DIR}/app/:/app/ --rm -it riscv bash
