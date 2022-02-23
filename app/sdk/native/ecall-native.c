2460e4af340513ca1d69f2b90f401b2bc33d278c
greenknot <greenknot@users.noreply.github.com>
2022-02-23 14:53:21 +0100

    sdk: native: add xsend

--- app/sdk/native/ecall-native.c
+++ app/sdk/native/ecall-native.c
@@ -1,39 +1,67 @@
 #include <err.h>
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "ecall.h"
 #include "sdk/sdk.h"
 
-void readall(int fd, void *buf, size_t count)
+static void readall(int fd, void *buf, size_t count)
 {
-	uint8_t *p;
-	ssize_t n;
-	size_t i;
+    uint8_t *p;
+    ssize_t n;
+    size_t i;
 
-	p = buf;
-	i = count;
+    p = buf;
+    i = count;
 
-	while (i > 0) {
-		n = read(fd, p, i);
-		if (n == 0) {
-            err(1, "readall: read failed");
-		} else if (n == -1) {
-			if (errno == EINTR) {
-				continue;
+    while (i > 0) {
+        n = read(fd, p, i);
+        if (n == 0) {
+            errx(1, "readall: connection closed");
+        } else if (n == -1) {
+            if (errno == EINTR) {
+                continue;
             } else {
                 err(1, "readall: read failed");
             }
-		}
-		i -= n;
-		p += n;
-	}
+        }
+        i -= n;
+        p += n;
+    }
+}
+
+static void writeall(const uint8_t *buffer, size_t size)
+{
+    const char *p;
+    ssize_t i;
+
+    p = buffer;
+    while (size > 0) {
+        i = write(STDOUT_FILENO, p, size);
+        if (i == 0) {
+            err(1, "ecall_xsend: write failed");
+        } else if (i == -1) {
+            if (errno == EINTR) {
+                continue;
+            } else {
+                err(1, "ecall_xsend: write failed");
+            }
+        }
+        size -= i;
+        p += i;
+    }
 }
 
 void ecall_xsend(const uint8_t *buffer, size_t size)
 {
+    uint32_t n = size;
+
+    writeall((uint8_t *)&n, sizeof(n));
+    writeall(buffer, size);
 }
+
 
 size_t ecall_xrecv(uint8_t *buffer, size_t size)
 {
@@ -52,6 +80,8 @@ size_t ecall_xrecv(uint8_t *buffer, size_t size)
 
 void ecall_sha256sum(const uint8_t *buffer, size_t size, uint8_t *digest)
 {
+    /* XXX */
+    memset(digest, 'a', 32);
 }
 
 void ecall_screen_update(void)
@@ -68,6 +98,7 @@ void ecall_bagl_hal_draw_bitmap_within_rect(int x, int y, unsigned int width, un
 
 int ecall_wait_button(void)
 {
+    /* XXX */
     return 1;
 }
 
