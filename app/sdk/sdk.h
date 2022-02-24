43b8507b21a0e93dbd7109b3cab73a83e40b3404
greenknot <greenknot@users.noreply.github.com>
2022-02-24 15:47:10 +0100

    sdk: add xmalloc

--- app/sdk/sdk.h
+++ app/sdk/sdk.h
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 void xsend(const uint8_t *buffer, size_t size);
 size_t xrecv(uint8_t *buffer, size_t size);
@@ -16,3 +17,12 @@ void app_loading_start(void);
 bool app_loading_stop(void);
 
 __attribute__((noreturn)) void fatal(char *msg);
+
+static inline void *xmalloc(size_t size)
+{
+    void *p = malloc(size);
+    if (p == NULL) {
+        fatal("malloc failed");
+    }
+    return p;
+}
