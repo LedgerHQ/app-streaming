40d1279a6804b7ad9413f2f9f47c195e5cf4ddc2
greenknot <greenknot@users.noreply.github.com>
2022-03-02 18:05:39 +0100

    app-ethereum: set path in the plugin tx

--- app/app-ethereum/plugin-ethereum.py
+++ app/app-ethereum/plugin-ethereum.py
@@ -11,6 +11,8 @@ from protobuf import message_pb2  # noqa: E402
 class Plugin:
     def __init__(self):
         self.name = "ethereum"
+        # 44'/60'/0/0
+        self.path = [0x8000002c, 0x8000003c, 0, 0]
 
     def get_version_prepare_request(self):
         get_version = message_pb2.RequestGetVersion()
@@ -28,8 +30,7 @@ class Plugin:
 
     def get_pubkey_prepare_request(self):
         get_pubkey = message_pb2.RequestGetPubKey()
-        # 44'/60'/0/0
-        get_pubkey.path.extend([0x8000002c, 0x8000003c, 0, 0])
+        get_pubkey.path.extend(self.path)
         get_pubkey.confirm = False
         get_pubkey.get_chain_code = True
         message = message_pb2.Request()
@@ -45,6 +46,7 @@ class Plugin:
 
     def sign_tx_prepare_request(self):
         sign_tx = message_pb2.RequestSignTx()
+        sign_tx.path.extend(self.path)
         sign_tx.raw_tx = bytes.fromhex("f080843b9aca0083015f90946b477781b0e68031109f21887e6b5afeaaeb002b808c5468616e6b732c206d616e21038080")
         sign_tx.chain_id = 1
         message = message_pb2.Request()
