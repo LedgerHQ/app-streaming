4817f82848b8a2987238f659b45dc1313f8d9c09
greenknot <greenknot@users.noreply.github.com>
2022-03-15 12:42:31 +0100

    host: replace tuple with a named collection

--- host/stream.py
+++ host/stream.py
@@ -9,15 +9,19 @@ import logging
 import os
 import sys
 
+from collections import namedtuple
 from construct import Bytes, Int8ul, Int16ul, Int32ul, Struct
 from encryption import EncryptedApp
 from merkletree import Entry, MerkleTree
-from typing import Any, Dict, Tuple
+from typing import Any, Dict
 from server import Server
 
 from ledgerwallet.client import LedgerClient
 from ledgerwallet.transport import enumerate_devices
 from ledgerwallet.utils import serialize
+
+
+Apdu = namedtuple("Apdu", "status data")
 
 
 class Page:
@@ -34,10 +38,6 @@ class Page:
 
 
 def import_ledgerwallet(use_speculos: bool) -> None:
-    global LedgerClient
-    global enumerate_devices
-    global serialize
-
     if use_speculos:
         os.environ["LEDGER_PROXY_ADDRESS"] = "127.0.0.1"
         os.environ["LEDGER_PROXY_PORT"] = "9999"
@@ -57,12 +57,12 @@ def get_client() -> LedgerClient:
     return LedgerClient(devices[0], cla=CLA)
 
 
-def exchange(client: LedgerClient, ins: int, data=b"", p1=0, p2=0) -> Tuple[int, bytes]:
+def exchange(client: LedgerClient, ins: int, data=b"", p1=0, p2=0) -> Apdu:
     apdu = bytes([client.cla, ins, p1, p2])
     apdu += serialize(data)
     response = client.raw_exchange(apdu)
     status_word = int.from_bytes(response[-2:], "big")
-    return status_word, response[:-2]
+    return Apdu(status_word, response[:-2])
 
 
 class Stream:
@@ -113,18 +113,17 @@ class Stream:
             self.pages[addr].update(data, mac, iv)
             self.merkletree.update(Entry.from_values(addr, iv))
 
-    def init_app(self) -> Tuple[int, bytes]:
-        data = b"\x00" * 3  # for alignment
-        data += self.manifest
-        status_word, data = exchange(client, ins=0x00, data=data)
-        return status_word, data
-
     @staticmethod
     def _parse_request(struct: Struct, data: bytes) -> Any:
         assert len(data) == struct.sizeof()
         return struct.parse(data)
 
-    def handle_read_access(self, data: bytes) -> Tuple[int, bytes]:
+    def init_app(self) -> Apdu:
+        # pad with 3 bytes because of alignment
+        data = b"\x00" * 3 + self.manifest
+        return exchange(client, ins=0x00, data=data)
+
+    def handle_read_access(self, data: bytes) -> Apdu:
         request = Stream._parse_request(Struct("addr" / Int32ul), data)
         # 1. addr was received
         logger.debug(f"read access: {request.addr:#x}")
@@ -132,36 +131,36 @@ class Stream:
         # 2. send encrypted page data
         page = self._get_page(request.addr)
         assert len(page.data) == Stream.PAGE_SIZE
-        status_word, data = exchange(client, 0x01, data=page.data[1:], p2=page.data[0])
-        assert status_word == 0x6102
+        apdu = exchange(client, 0x01, data=page.data[1:], p2=page.data[0])
+        assert apdu.status == 0x6102
 
         # 4. send iv and mac
         data = Struct("iv" / Int32ul, "mac" / Bytes(32)).build(dict(iv=page.iv, mac=page.mac))
-        status_word, data = exchange(client, 0x02, data=data)
+        apdu = exchange(client, 0x02, data=data)
 
         # 5. send merkle proof
         if not page.read_only:
-            assert status_word == 0x6103
+            assert apdu.status == 0x6103
             entry, proof = self.merkletree.get_proof(request.addr)
             assert entry.addr == request.addr
             assert entry.counter == page.iv
 
             # TODO: handle larger proofs
             assert len(proof) <= 250
-            status_word, data = exchange(client, 0x03, data=proof)
+            apdu = exchange(client, 0x03, data=proof)
 
-        return status_word, data
+        return apdu
 
-    def handle_write_access(self, data: bytes) -> Tuple[int, bytes]:
+    def handle_write_access(self, data: bytes) -> Apdu:
         # 1. encrypted page data was received
         assert len(data) == Stream.PAGE_SIZE
         page_data = data
 
         # 2. receive addr, iv and mac
-        status_word, data = exchange(client, 0x01)
-        assert status_word == 0x6202
+        apdu = exchange(client, 0x01)
+        assert apdu.status == 0x6202
 
-        request = Stream._parse_request(Struct("addr" / Int32ul, "iv" / Int32ul, "mac" / Bytes(32)), data)
+        request = Stream._parse_request(Struct("addr" / Int32ul, "iv" / Int32ul, "mac" / Bytes(32)), apdu.data)
         assert (request.addr & Stream.PAGE_MASK_INVERT) == 0
         logger.debug(f"write access: {request.addr:#x} {request.iv:#x} {request.mac.hex()}")
 
@@ -180,10 +179,9 @@ class Stream:
         # TODO: handle larger proofs
         assert len(proof) <= 250
 
-        status_word, data = exchange(client, 0x02, data=proof)
-        return status_word, data
+        return exchange(client, 0x02, data=proof)
 
-    def handle_send_buffer(self, data: bytes) -> Tuple[int, bytes]:
+    def handle_send_buffer(self, data: bytes) -> Apdu:
         logger.info(f"got buffer {data}")
 
         request = Stream._parse_request(Struct("data" / Bytes(249), "size" / Int8ul, "counter" / Int32ul), data)
@@ -201,10 +199,9 @@ class Stream:
             self.send_buffer = b""
             self.send_buffer_counter = 0
 
-        status_word, data = exchange(client, 0x00, data=b"")
-        return status_word, data
+        return exchange(client, 0x00, data=b"")
 
-    def handle_recv_buffer(self, data: bytes) -> Tuple[int, bytes]:
+    def handle_recv_buffer(self, data: bytes) -> Apdu:
         request = Stream._parse_request(Struct("counter" / Int32ul, "maxsize" / Int16ul), data)
 
         if len(self.recv_buffer) == 0:
@@ -231,8 +228,8 @@ class Stream:
             buf = buf[1:]
         else:
             p2 = 0x00
-        status_word, data = exchange(client, 0x00, p1=last, p2=p2, data=buf)
-        return status_word, data
+
+        return exchange(client, 0x00, p1=last, p2=p2, data=buf)
 
     def handle_exit(self, data: bytes) -> None:
         request = Stream._parse_request(Struct("code" / Int32ul), data)
@@ -263,23 +260,23 @@ if __name__ == "__main__":
     stream = Stream(args.app)
     client = get_client()
 
-    status_word, data = stream.init_app()
+    apdu = stream.init_app()
     while True:
-        logger.debug(f"[<] {status_word:#06x} {data[:4].hex()}")
-        if status_word == 0x6101:
-            status_word, data = stream.handle_read_access(data)
-        elif status_word == 0x6201:
-            status_word, data = stream.handle_write_access(data)
-        elif status_word == 0x6301:
-            status_word, data = stream.handle_send_buffer(data)
-        elif status_word == 0x6401:
-            status_word, data = stream.handle_recv_buffer(data)
-        elif status_word == 0x6501:
-            stream.handle_exit(data)
+        logger.debug(f"[<] {apdu.status:#06x} {apdu.data[:8].hex()}...")
+        if apdu.status == 0x6101:
+            apdu = stream.handle_read_access(apdu.data)
+        elif apdu.status == 0x6201:
+            apdu = stream.handle_write_access(apdu.data)
+        elif apdu.status == 0x6301:
+            apdu = stream.handle_send_buffer(apdu.data)
+        elif apdu.status == 0x6401:
+            apdu = stream.handle_recv_buffer(apdu.data)
+        elif apdu.status == 0x6501:
+            stream.handle_exit(apdu.data)
             break
-        elif status_word == 0x6601:
-            stream.handle_fatal(data)
+        elif apdu.status == 0x6601:
+            stream.handle_fatal(apdu.data)
             break
         else:
-            logger.error(f"unexpected status {status_word:#06x}, {data}")
+            logger.error(f"unexpected status {apdu.status:#06x}, {apdu.data}")
             assert False
