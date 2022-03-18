0a310860184940dc5260c82e5f047a82abb831b4
greenknot <greenknot@users.noreply.github.com>
2022-03-18 15:31:25 +0100

    vm: get rid of flow for loading UX

--- vm/src/loading.c
+++ vm/src/loading.c
@@ -5,94 +5,70 @@
 #include "ux.h"
 
 #include "os_io_seproxyhal.h"
+#include "os_screen.h"
 
 #define TICKER_THRESHOLD       175
 #define INSTRUCTIONS_THRESHOLD 2000
+#define NUMBER_OF_ICONS        8
 
 #define ARRAYLEN(array) (sizeof(array) / sizeof(array[0]))
+
+#if (BAGL_WIDTH==128 && BAGL_HEIGHT==64)
+#define TEXT_Y 44
+#elif (BAGL_WIDTH==128 && BAGL_HEIGHT==32)
+#define TEXT_Y 28
+#else
+#error "BAGL_WIDTH/BAGL_HEIGHT not defined"
+#endif
+
+const bagl_component_t icon_component = {
+#if (BAGL_WIDTH==128 && BAGL_HEIGHT==64)
+    BAGL_ICON, 0x10, 57, 17, 14, 14, 0, 0, 0, 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_REGULAR_11px|BAGL_FONT_ALIGNMENT_CENTER, 0
+#elif (BAGL_WIDTH==128 && BAGL_HEIGHT==32)
+    BAGL_ICON, 0x10, 56,  2, 16, 16, 0, 0, 0, 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_REGULAR_11px|BAGL_FONT_ALIGNMENT_CENTER, 0
+#else
+#error "BAGL_WIDTH/BAGL_HEIGHT not defined"
+#endif
+};
+
+static const bagl_component_t status_component = {
+    BAGL_LABELINE, 0x11, 0, TEXT_Y, 128, 32, 0, 0, 0, 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_REGULAR_11px|BAGL_FONT_ALIGNMENT_CENTER, 0
+};
 
 static size_t app_loading_counter;
 static size_t app_loading_ticker;
 static bool app_loading;
+static size_t app_loading_step;
 
-typedef struct ux_layout_p_params_s {
-    const bagl_icon_details_t* icon;
-	const char* line1;
-} ux_layout_p_params_t;
-
-
-const bagl_element_t ux_layout_p_elements[] = {
-#if (BAGL_WIDTH==128 && BAGL_HEIGHT==64)
-  {{BAGL_RECTANGLE                      , 0x00,   0,   0, 128,  64, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF, 0, 0}, NULL},
-
-  {{BAGL_ICON                           , 0x10,  57,  17,  14,  14, 0, 0, 0        , 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_REGULAR_11px|BAGL_FONT_ALIGNMENT_CENTER, 0  }, NULL },
-  {{BAGL_LABELINE                       , 0x11,   0,  44, 128,  32, 0, 0, 0        , 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_REGULAR_11px|BAGL_FONT_ALIGNMENT_CENTER, 0  }, NULL },
-#elif (BAGL_WIDTH==128 && BAGL_HEIGHT==32)
-  // erase
-  {{BAGL_RECTANGLE                      , 0x00,   0,   0, 128,  32, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF, 0, 0}, NULL},
-
-  {{BAGL_ICON                           , 0x10,  56,  2,  16,  16, 0, 0, 0        , 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_REGULAR_11px|BAGL_FONT_ALIGNMENT_CENTER, 0  }, NULL },
-  {{BAGL_LABELINE                       , 0x11,   0, 28, 128,  32, 0, 0, 0        , 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_REGULAR_11px|BAGL_FONT_ALIGNMENT_CENTER, 0  }, NULL },
-#else
-  #error "BAGL_WIDTH/BAGL_HEIGHT not defined"
-#endif
-};
-
-static const bagl_element_t* ux_layout_p_prepro(const bagl_element_t* element)
+static void display_next_icon(void)
 {
-    const ux_layout_p_params_t* params = (const ux_layout_p_params_t*)ux_stack_get_current_step_params();
-
-    memmove(&G_ux.tmp_element, element, sizeof(bagl_element_t));
-
-    switch (element->component.userid) {
-    case 0x10:
-        G_ux.tmp_element.text = (const char*)params->icon;
-        break;
-    case 0x11:
-  		G_ux.tmp_element.text = params->line1;
+    /* I don't know how to use PIC */
+    const bagl_icon_details_t *loading_icon;
+    switch (app_loading_step) {
+    default:
+    case 0: loading_icon = &C_icon_loading_1; break;
+    case 1: loading_icon = &C_icon_loading_2; break;
+    case 2: loading_icon = &C_icon_loading_3; break;
+    case 3: loading_icon = &C_icon_loading_4; break;
+    case 4: loading_icon = &C_icon_loading_5; break;
+    case 5: loading_icon = &C_icon_loading_6; break;
+    case 6: loading_icon = &C_icon_loading_7; break;
+    case 7: loading_icon = &C_icon_loading_8; break;
     }
 
-    return &G_ux.tmp_element;
+    io_seproxyhal_display_icon((bagl_component_t *)&icon_component,
+                               (bagl_icon_details_t *)loading_icon);
+    screen_update();
+
+    app_loading_step = (app_loading_step + 1) % NUMBER_OF_ICONS;
 }
-
-static void ux_layout_p_init(unsigned int stack_slot)
-{
-    ux_stack_init(stack_slot);
-    G_ux.stack[stack_slot].element_arrays[0].element_array = ux_layout_p_elements;
-    G_ux.stack[stack_slot].element_arrays[0].element_array_count = ARRAYLEN(ux_layout_p_elements);
-    G_ux.stack[stack_slot].element_arrays_count = 1;
-    G_ux.stack[stack_slot].screen_before_element_display_callback = ux_layout_p_prepro;
-    G_ux.stack[stack_slot].button_push_callback = ux_flow_button_callback;
-    ux_stack_display(stack_slot);
-}
-
-static char loading_status[32];
-
-UX_STEP_NOCB(ux_loading_1_step, p, {&C_icon_loading_1, loading_status});
-UX_STEP_NOCB(ux_loading_2_step, p, {&C_icon_loading_2, loading_status});
-UX_STEP_NOCB(ux_loading_3_step, p, {&C_icon_loading_3, loading_status});
-UX_STEP_NOCB(ux_loading_4_step, p, {&C_icon_loading_4, loading_status});
-UX_STEP_NOCB(ux_loading_5_step, p, {&C_icon_loading_5, loading_status});
-UX_STEP_NOCB(ux_loading_6_step, p, {&C_icon_loading_6, loading_status});
-UX_STEP_NOCB(ux_loading_7_step, p, {&C_icon_loading_7, loading_status});
-UX_STEP_NOCB(ux_loading_8_step, p, {&C_icon_loading_8, loading_status});
-
-UX_FLOW(ux_loading_flow,
-        &ux_loading_1_step,
-        &ux_loading_2_step,
-        &ux_loading_3_step,
-        &ux_loading_4_step,
-        &ux_loading_5_step,
-        &ux_loading_6_step,
-        &ux_loading_7_step,
-        &ux_loading_8_step,
-        FLOW_LOOP);
 
 void sys_app_loading_start(uint32_t status_addr)
 {
     app_loading = true;
     app_loading_counter = 0;
 
+    char loading_status[32];
     if (status_addr == 0) {
         loading_status[0] = '\x00';
     } else {
@@ -102,9 +78,13 @@ void sys_app_loading_start(uint32_t status_addr)
         loading_status[sizeof(loading_status) - 1] = '\x00';
     }
 
-    memset(&G_ux, 0, sizeof(G_ux));
-    ux_stack_push();
-    ux_flow_init(0, ux_loading_flow, NULL);
+    /* erase screen */
+    bagl_draw_bg(0);
+
+    /* draw text */
+    bagl_draw_with_context(&status_component, loading_status, strlen(loading_status), BAGL_ENCODING_LATIN1);
+
+    display_next_icon();
 }
 
 bool sys_app_loading_stop(void)
@@ -139,7 +119,7 @@ void app_loading_update_ui(bool host_exchange)
     }
 
     if (update_ui) {
-        ux_flow_next();
+        display_next_icon();
     }
 
 }
