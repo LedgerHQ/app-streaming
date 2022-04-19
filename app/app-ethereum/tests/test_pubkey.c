92c97010af7df218c8e3c7cc5bfaa43ae329826f
greenknot <greenknot@users.noreply.github.com>
2022-04-19 14:36:06 +0200

    app-eth: fix pubkey

--- app/app-ethereum/tests/test_pubkey.c
+++ app/app-ethereum/tests/test_pubkey.c
@@ -22,12 +22,12 @@ static void test_pubkey(void **state __attribute__((unused)))
     uint64_t chain_id = 1;
     cx_ecfp_public_key_t publicKey;
 
-    unhex("00691cd378a84c710992412b53df9f5578fbcc6da3af820d2381a4b079f633948bdd789024b52f3eb8fb842c"
-          "f86704b4b124fbe7cc9ea175506d658938819a674e",
+    unhex("0400691cd378a84c710992412b53df9f5578fbcc6da3af820d2381a4b079f633948bdd789024b52f3eb8fb84"
+          "2cf86704b4b124fbe7cc9ea175506d658938819a674e",
           publicKey.W, sizeof(publicKey.W));
     getEthAddressStringFromKey(&publicKey, address, chain_id);
 
-    assert_string_equal(address, "C9ACd1a02dDd108b877424346dc5CBDfe4714360");
+    assert_string_equal(address, "48Cea1ae35d23fB6DbB682dc94b3Bd79e80c657E");
 }
 
 /* https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1191.md */
@@ -36,15 +36,15 @@ static void test_pubkey_eip_1191(void **state __attribute__((unused)))
     char address[41];
     cx_ecfp_public_key_t publicKey;
 
-    unhex("00691cd378a84c710992412b53df9f5578fbcc6da3af820d2381a4b079f633948bdd789024b52f3eb8fb842c"
-          "f86704b4b124fbe7cc9ea175506d658938819a674e",
+    unhex("0400691cd378a84c710992412b53df9f5578fbcc6da3af820d2381a4b079f633948bdd789024b52f3eb8fb84"
+          "2cf86704b4b124fbe7cc9ea175506d658938819a674e",
           publicKey.W, sizeof(publicKey.W));
 
     getEthAddressStringFromKey(&publicKey, address, 30);
-    assert_string_equal(address, "c9acD1A02DDd108b877424346Dc5cBDFe4714360");
+    assert_string_equal(address, "48CEa1aE35d23fB6dbb682dc94b3bd79e80c657e");
 
     getEthAddressStringFromKey(&publicKey, address, 31);
-    assert_string_equal(address, "C9acD1a02DDd108b877424346dC5cbDfE4714360");
+    assert_string_equal(address, "48cea1AE35d23FB6dbb682dc94B3bD79e80C657E");
 }
 
 int main(void)
