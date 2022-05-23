87566fa8a2a58cf2977c311522bf7c9fcd206597
greenknot <greenknot@users.noreply.github.com>
2022-05-23 15:47:40 +0200

    app: replace plugin files with standalone scripts

---
+++ app/app-sha256/sha256.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import hashlib
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "host"))
+
+import stream  # noqa: E402
+
+
+if __name__ == "__main__":
+    parser = stream.get_stream_arg_parser()
+    args = parser.parse_args()
+
+    with stream.Streamer(args) as streamer:
+        to_hash = b"a" * 1024
+
+        rsize = len(to_hash).to_bytes(4, "little")
+        data = streamer.exchange(rsize)
+        assert data == b"ok"
+
+        data = streamer.exchange(to_hash)
+        print(f"hexdigest: {data!r}")
+        assert data is not None
+        assert hashlib.sha256(to_hash).hexdigest() == data.decode()
--- app/app-swap/swap.py
+++ app/app-swap/swap.py
@@ -4,16 +4,16 @@ import sys
 # ensure that protobuf/message_pb2.py will be found
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "host"))
+
 # apt install python3-protobuf
 from protobuf import message_pb2  # noqa: E402
 
+import stream  # noqa: E402
 
-class Plugin:
-    def __init__(self):
-        self.name = "swap"
-        self.device_id = None
 
-    def init_swap_prepare_request(self):
+class Swap:
+    def init_swap_prepare_request(self) -> bytes:
         init_swap = message_pb2.RequestInitSwap()
         message = message_pb2.Request()
         message.init_swap.CopyFrom(init_swap)
@@ -28,7 +28,7 @@ class Plugin:
         self.device_id = response.init_swap.device_id
         return response
 
-    def swap_prepare_request(self):
+    def swap_prepare_request(self) -> bytes:
         """get_version = message_pb2.RequestGetVersion()
         message = message_pb2.Request()
         message.get_version.CopyFrom(get_version)
@@ -45,3 +45,18 @@ class Plugin:
         assert response.WhichOneof("message_oneof") == "swap"
         print(f"response: {response.swap}")
         return response
+
+
+if __name__ == "__main__":
+    parser = stream.get_stream_arg_parser()
+    parser.add_argument("--action", default="swap", choices=["swap"])
+    args = parser.parse_args()
+
+    with stream.Streamer(args) as streamer:
+        swap = Swap()
+
+        data = streamer.exchange(swap.init_swap_prepare_request())
+        swap.init_swap_parse_response(data)
+
+        data = streamer.exchange(swap.swap_prepare_request())
+        swap.swap_parse_response(data)
