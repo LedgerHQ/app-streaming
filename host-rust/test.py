4a09d882764f9dbc2d6433512cac5a783eb89629
greenknot <greenknot@users.noreply.github.com>
2022-07-12 18:17:42 +0200

    wip

--- host-rust/test.py
+++ host-rust/test.py
@@ -9,15 +9,7 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../ho
 import libstreaming as streaming
 import comm as host_comm
 
-class Comm:
-    def exchange_(self, data: bytes) -> bytes:
-        print(data)
-        print(f"{data.hex()}")
-        return data
-
-
 if __name__ == "__main__":
-    comm = Comm()
     with host_comm.get_client(use_speculos=True) as comm:
         data = streaming.get_pubkey("blah", comm)
         print(data)
