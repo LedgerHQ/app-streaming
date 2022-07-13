52e25af8c2035e3b6852774640ec8c660298a4b1
greenknot <greenknot@users.noreply.github.com>
2022-07-13 09:01:19 +0200

    Python: working

--- host-rust/src/lib/lib.rs
+++ host-rust/src/lib/lib.rs
@@ -14,44 +14,33 @@ pub mod comm;
 pub mod manifest;
 pub mod serialization;
 
-use cpython::{py_fn, py_module_initializer, PyBytes, PyObject, PyResult, Python};
+use cpython::{py_fn, py_module_initializer, PyBytes, PyNone, PyObject, PyResult, Python};
 
-// PYTHONPATH=$(pwd)/target/debug/ python3
-
-// add bindings to the generated python module
-// N.B: names: "rust2py" must be the name of the `.so` or `.pyd` file
 py_module_initializer!(libstreaming, |py, m| {
     m.add(py, "__doc__", "This module is implemented in Rust.")?;
-    m.add(
-        py,
-        "sum_as_string",
-        py_fn!(py, sum_as_string_py(a: i64, b: i64)),
-    )?;
     m.add(
         py,
         "get_pubkey",
         py_fn!(py, get_pubkey_py(path: &str, comm: &PyObject)),
     )?;
+    m.add(
+        py,
+        "device_sign_app",
+        py_fn!(py, device_sign_app_py(path: &str, comm: &PyObject)),
+    )?;
     Ok(())
 });
-
-// logic implemented as a normal rust function
-fn sum_as_string(a: i64, b: i64) -> String {
-    format!("{}", a + b)
-}
-
-// rust-cpython aware function. All of our python interface could be
-// declared in a separate module.
-// Note that the py_fn!() macro automatically converts the arguments from
-// Python objects to Rust values; and the Rust return value back into a Python object.
-fn sum_as_string_py(_: Python, a: i64, b: i64) -> PyResult<String> {
-    let out = sum_as_string(a, b);
-    Ok(out)
-}
 
 fn get_pubkey_py(py: Python, path: &str, comm: &PyObject) -> PyResult<PyBytes> {
     let app = app::App::from_zip(path);
     let comm = comm::Comm::new(py, comm);
     let pubkey = app.get_pubkey(&comm);
     Ok(PyBytes::new(py, &pubkey))
+}
+
+fn device_sign_app_py(py: Python, path: &str, comm: &PyObject) -> PyResult<PyNone> {
+    let mut app = app::App::from_zip(path);
+    let comm = comm::Comm::new(py, comm);
+    app.device_sign_app(&comm);
+    Ok(PyNone)
 }
--- host-rust/test.py
+++ host-rust/test.py
@@ -13,3 +13,4 @@ if __name__ == "__main__":
     with host_comm.get_client(use_speculos=True) as comm:
         data = streaming.get_pubkey("/tmp/app.zip", comm)
         print(f"pubkey: {data.hex()}")
+        streaming.device_sign_app("/tmp/app.zip", comm)
